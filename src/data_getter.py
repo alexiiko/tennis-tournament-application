f5cdf5b789e349d27f5bd3bad8d84a10d3776b6c
alexiiko <alexiiko.msl@gmail.com>
2025-02-01 10:18:38 +0100

    changed the tournament variables to a dict

--- src/data_getter.py
+++ src/data_getter.py
@@ -1,9 +1,9 @@
 import pyautogui as pag
-import constants
 import keyboard as kb
 import pyperclip
+import constants
 from time import sleep
-from typing import Tuple
+from typing import Dict, Tuple
 
 '''
 pag.hotkey("super", "shift", "2")
@@ -25,11 +25,10 @@ def copy_text_and_return_as_variable(location_left: Tuple[int,int], location_rig
     return str(pyperclip.paste()).replace("\n", "").replace("\r", "").replace("\t", "")
 
 
-def get_tournament_location_and_organizer() -> Tuple[str, str, str]:
+def get_tournament_location_and_organizer() -> Dict[str, str]:
     pag.scroll(constants.COURT_FACILITY_SITE_OFFSET)
     pag.moveTo(1060,1168)
     pag.scroll(constants.GET_TO_COURT_FACILITY)
-    sleep(0.3)
     pag.click()
 
     sleep(1)
@@ -38,18 +37,18 @@ def get_tournament_location_and_organizer() -> Tuple[str, str, str]:
     tournament_street = copy_text_and_return_as_variable(constants.STREET_LEFT, constants.STREET_RIGHT)
     tournament_organizer = copy_text_and_return_as_variable(constants.TOURNAMENT_ORGANIZER_LEFT, constants.TOURNAMENT_ORGANIZER_RIGHT)
 
-    return tournament_plz, tournament_street, tournament_organizer
+    return {"tournament_plz": tournament_plz, "tournament_street": tournament_street, "tournament_organizer": tournament_organizer}
 
 
-def get_tournament_name() -> str:
+def get_tournament_name() -> Dict[str, str]:
     pag.scroll(constants.COURT_FACILITY_SITE_OFFSET)
 
     tournament_title = copy_text_and_return_as_variable(constants.TITLE_LEFT, constants.TITLE_RIGHT)
 
-    return tournament_title
+    return {"tournament_title": tournament_title}
 
 
-def get_tournament_dates() -> Tuple[str, str, str, str]:
+def get_tournament_dates() -> Dict[str, str]:
     pag.scroll(constants.COURT_FACILITY_SITE_OFFSET)
 
     tournament_date = copy_text_and_return_as_variable(constants.DATE_LEFT, constants.DATE_RIGHT)
@@ -57,14 +56,11 @@ def get_tournament_dates() -> Tuple[str, str, str, str]:
     tournament_registration_end = copy_text_and_return_as_variable(constants.REGISTRATION_DEADLINE_LEFT, constants.REGISTRATION_DEADLINE_RIGHT)
     tournament_draw = copy_text_and_return_as_variable(constants.DRAW_LEFT, constants.DRAW_RIGHT)
 
-    return tournament_date, tournament_registration_start, tournament_registration_end, tournament_draw
+    return {"tournament_date": tournament_date, "tournament_registration_start": tournament_registration_start, "tournament_registration_end": tournament_registration_end, "tournament_draw": tournament_draw}
 
 
 while True:
     if kb.is_pressed("a"):
-        print(get_tournament_location_and_organizer())
-        print()
-        print(get_tournament_name())
-        print()
-        print(get_tournament_dates())
+        tournament_data = get_tournament_location_and_organizer() | get_tournament_dates() | get_tournament_name()
+        print(tournament_data)
         sleep(0.5)
