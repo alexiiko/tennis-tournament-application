1ffff9599e293b5b0f6fac7ff94997effa53c408
alexiiko <alexiiko.msl@gmail.com>
2025-02-02 14:29:52 +0100

    changed the json structure, added the function to scroll through the different tournaments available, added the simulation that the platform gets opened and the tournaments get scrolled through, wrapped the data functions into one big function

--- src/data_getter.py
+++ src/data_getter.py
@@ -4,16 +4,8 @@ import pyperclip
 import constants
 import json
 from time import sleep
-from typing import Dict, Tuple
+from typing import Tuple, Dict
 
-'''
-pag.hotkey("super", "shift", "2")
-
-sleep(2)
-
-pag.write("https://spieler.tennis.de/group/guest/turniere")
-pag.press("enter")
-'''
 
 # helper function for saving code lines
 def copy_text_and_return_as_variable(location_left: Tuple[int,int], location_right: Tuple[int,int,]):
@@ -26,42 +18,84 @@ def copy_text_and_return_as_variable(location_left: Tuple[int,int], location_rig
     return str(pyperclip.paste()).replace("\n", "").replace("\r", "").replace("\t", "")
 
 
-def get_tournament_location_and_organizer() -> Dict[str, str]:
-    pag.scroll(constants.COURT_FACILITY_SITE_OFFSET)
-    pag.moveTo(1060,1168)
-    pag.scroll(constants.GET_TO_COURT_FACILITY)
-    pag.click()
+def get_tournament_data() -> str:
+    def get_tournament_location() -> Dict[str, Dict[str, str]]:
+        pag.scroll(-10000) # get to bottom of the page
+        sleep(0.5)
+        pag.scroll(constants.TOURNAMENT_INFO_OFFSET)
+        sleep(0.5)
+        pag.click(constants.COURT_FACILITY_BUTTON)
+        tournament_plz = copy_text_and_return_as_variable(constants.LEFT_TOURNAMENT_PLZ, constants.RIGHT_TOURNAMENT_PLZ)
+        tournament_street = copy_text_and_return_as_variable(constants.LEFT_TOURNAMENT_STREET, constants.RIGHT_TOURNAMENT_STREET)
 
-    sleep(1)
-    
-    tournament_plz = copy_text_and_return_as_variable(constants.PLZ_LOCATION_LEFT, constants.PLZ_LOCATION_RIGHT)
-    tournament_street = copy_text_and_return_as_variable(constants.STREET_LEFT, constants.STREET_RIGHT)
-    tournament_organizer = copy_text_and_return_as_variable(constants.TOURNAMENT_ORGANIZER_LEFT, constants.TOURNAMENT_ORGANIZER_RIGHT)
-
-    return {"tournament_plz": tournament_plz, "tournament_street": tournament_street, "tournament_organizer": tournament_organizer}
+        return {"tournament_location": {"tournament_plz": tournament_plz, "tournament_street": tournament_street}}
 
 
-def get_tournament_name() -> Dict[str, str]:
-    pag.scroll(constants.COURT_FACILITY_SITE_OFFSET)
+    def get_tournament_dates() -> Dict[str, Dict[str, str]]:
+        tournament_date = copy_text_and_return_as_variable(constants.LEFT_TOURNAMENT_DATE, constants.RIGHT_TOURNAMENT_DATE)
+        # if there is registration start button (check with screenshot) then add it into here 
+        tournament_registration_end = copy_text_and_return_as_variable(constants.LEFT_REGISTRATION_END, constants.RIGHT_REGISTRATION_END)
+        tournament_draw_date = copy_text_and_return_as_variable(constants.LEFT_DRAW_DATE, constants.RIGHT_DRAW_DATE)
 
-    tournament_title = copy_text_and_return_as_variable(constants.TITLE_LEFT, constants.TITLE_RIGHT)
-
-    return {"tournament_title": tournament_title}
+        return {"tournament_dates": {"tournament_date": tournament_date, "tournament_registration_end": tournament_registration_end, "tournament_draw_date": tournament_draw_date}}
 
 
-def get_tournament_dates() -> Dict[str, str]:
-    pag.scroll(constants.COURT_FACILITY_SITE_OFFSET)
+    def get_tournament_title() -> Dict[str, str]:
+        pag.scroll(5000)
+        sleep(0.5)
+        tournament_title = copy_text_and_return_as_variable(constants.LEFT_TOURNAMENT_TITLE, constants.RIGHT_TOURNAMENT_TITLE)
+        return {"tournament_title": tournament_title}
 
-    tournament_date = copy_text_and_return_as_variable(constants.DATE_LEFT, constants.DATE_RIGHT)
-    tournament_registration_start = copy_text_and_return_as_variable(constants.REGISTRATION_START_LEFT, constants.REGISTRATION_START_RIGHT)
-    tournament_registration_end = copy_text_and_return_as_variable(constants.REGISTRATION_DEADLINE_LEFT, constants.REGISTRATION_DEADLINE_RIGHT)
-    tournament_draw = copy_text_and_return_as_variable(constants.DRAW_LEFT, constants.DRAW_RIGHT)
 
-    return {"tournament_date": tournament_date, "tournament_registration_start": tournament_registration_start, "tournament_registration_end": tournament_registration_end, "tournament_draw": tournament_draw}
+    def get_tournament_link() -> Dict[str, str]:
+        pag.moveTo(constants.TOURNAMENT_LINK)
+        pag.click()
+        pag.hotkey("ctrl", "c")
+        return {"tournament_link": str(pyperclip.paste())}
+
+    return json.dumps(get_tournament_title() | get_tournament_dates() | get_tournament_location() | get_tournament_link())
+
+
+def scroll_through_tournaments():
+    pag.click(constants.SEARCH_TOURNAMENTS_BUTTON)
+    sleep(2)
+    tournament_amount = int(copy_text_and_return_as_variable(constants.LEFT_TOURNAMENT_AMOUNT, constants.RIGHT_TOURNAMENT_AMOUNT))
+    pag.scroll(constants.SEE_TOURNAMENTS_OFFSET)
+    sleep(2)
+
+    for tournament_number in range(1, tournament_amount):
+        # if mehr turniere anzeigen on screen:
+        # click the mehr turniere anzeigen button 
+        # go to the top of the page 
+        # go down via the offsets
+        # else everything down below
+        pag.click(constants.SHOW_TOURNAMENT_BUTTON)
+        sleep(2)
+        get_tournament_data()
+        sleep(2)
+        pag.click(constants.PAGE_BACK_BUTTON)
+        sleep(2)
+        pag.moveTo(constants.SEARCH_TOURNAMENTS_BUTTON)
+        sleep(2)
+        pag.scroll(5000)
+        sleep(2)
+        pag.scroll(constants.SEE_TOURNAMENTS_OFFSET)
+        sleep(2)
+        pag.scroll(constants.NEXT_TOURNAMENT_OFFSET * tournament_number)
+        sleep(2)
 
 
 while True:
     if kb.is_pressed("a"):
-        tournament_data = json.dumps(get_tournament_location_and_organizer() | get_tournament_dates() | get_tournament_name())
+        tournament_data = get_tournament_data()
         print(tournament_data)
+        sleep(0.5)
+    elif kb.is_pressed("b"):
+        # open the platform
+        pag.hotkey("super", "shift", "2")
+        sleep(2)
+        pag.write("https://spieler.tennis.de/group/guest/turniere")
+        pag.press("enter")
+
+        scroll_through_tournaments()
         sleep(0.5)
