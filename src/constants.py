1d86b7643e38eaae832ef5e6a5bfd69ef676f443
alexiiko <alexiiko.msl@gmail.com>
2025-02-02 16:51:00 +0100

    added constants if RSP

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,7 @@
 TOURNAMENT_LINK = (1350, 125)
 PAGE_BACK_BUTTON = (40, 110)
 
+# search tournament page
 SEARCH_TOURNAMENTS_BUTTON = (2452, 1508)
 SEE_TOURNAMENTS_OFFSET = -1130  # Scroll down to only see tournaments
 
@@ -10,6 +11,8 @@ NEXT_TOURNAMENT_OFFSET = -380  # Offset to move to the next tournament
 LEFT_TOURNAMENT_AMOUNT = (2553, 1767)
 RIGHT_TOURNAMENT_AMOUNT = (2719, 1767)
 
+# tournament page
+# tournament dates if no registration start is provided
 LEFT_TOURNAMENT_TITLE = (88, 763)
 RIGHT_TOURNAMENT_TITLE = (2377, 763)
 
@@ -22,8 +25,17 @@ RIGHT_REGISTRATION_END = (1293, 1244)
 LEFT_DRAW_DATE = (701, 1357)
 RIGHT_DRAW_DATE = (1349, 1345)
 
-TOURNAMENT_INFO_OFFSET = 2190  # Scroll up from the bottom of the website
+# tournament dates if registration start is provided; RSP = RegistrationStartProvided
+LEFT_TOURNAMENT_DATE_RSP = (725, 880)
+RIGHT_TOURNAMENT_DATE_RSP = (1405, 880)
 
+LEFT_REGISTRATION_END_RSP = (690, 985)
+RIGHT_REGISTRATION_END_RSP = (1413, 988)
+
+LEFT_DRAW_DATE_RSP = (695, 1100)
+RIGHT_DRAW_DATE_RSP = (1046, 1100)
+
+# tournament location part
 COURT_FACILITY_BUTTON = (2648, 957)
 
 LEFT_TOURNAMENT_PLZ = (654, 1202)
