bdc8e20f8e017fdca44ef9d65c4f5a14333100a1
alexiiko <alexiiko.msl@gmail.com>
2025-03-04 20:57:07 +0100

    added file for working with the libsql database in go - added setup for connecting to the database - began functions for working with it

---
+++ src/libsql_database.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "database/sql"
+  "fmt"
+  "os"
+
+  _ "github.com/tursodatabase/libsql-client-go/libsql"
+  _ "github.com/mattn/go-sqlite3"
+)
+
+
+func readFromLocalDatabase() {
+
+}
+
+func insertLocalDataIntoRemoteDB() {
+
+}
+
+func main() {
+    dbToken := "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NDExMTY4NjUsImlkIjoiMDc1ZDQ5MTEtZDUzNS00Nzc5LWE0OTktMzZlYmE3YTEwNWQ2In0._5BR58POJ0aZYqciDxWVImP2IglQ1SjLXL7JJAobI6LZ27YesKCZAmXBksuiy3lk7BSRTg1ONFkwAMddwEisCA"
+
+    dbName := "turso-tournaments"
+
+    url := fmt.Sprintf("libsql://[%s].turso.io?authToken=[%s]", dbName, dbToken)
+
+    libDB, err := sql.Open("libsql", url)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "failed to open db %s: %s", url, err)
+        os.Exit(1)
+    }
+    readFromLocalDatabase()
+    insertLocalDataIntoRemoteDB()
+    defer libDB.Close()
+}
