d0b503752200df733bbc6f28c51fe424ab838cf9
alexiiko <alexiiko.msl@gmail.com>
2025-03-08 20:40:53 +0100

    removed reading from the local db - removed function that reads from the columns in the local db file - removed using this function in the main function - added new function for reading the lcoal json files

--- src/backend/go/dbConnection.go
+++ src/backend/go/dbConnection.go
@@ -1,88 +1,22 @@
 package main
 
 import (
-  "database/sql"
-  "fmt"
-  "os"
+	"database/sql"
+	"fmt"
+	"os"
 
-  "libsqlDB/dbCredentials"
+	"libsqlDB/dbCredentials"
 
-  _ "github.com/tursodatabase/libsql-client-go/libsql"
-  _ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 
-func readTournamentsFromLocalDatabase() map[string][]map[string]any {
-    localDB, err := sql.Open("sqlite3", "../tournaments.db")
-    if err != nil {
-        fmt.Println("Error connecting to the local database:", err)
-    }
-    defer localDB.Close()
-
-    ageClasses := map[string][]map[string]any{
-        "M18": {},
-        "M16": {},
-        "M14": {},
-        "M13": {},
-        "M12": {},
-        "M11": {},
-        "W18": {},
-        "W16": {},
-        "W14": {},
-        "W13": {},
-        "W12": {},
-        "W11": {},
-    }
-
-    for ageClass := range ageClasses {
-        rows, err := localDB.Query(fmt.Sprintf("SELECT * FROM %s", ageClass))
-        if err != nil {
-            fmt.Printf("Error retrieving data from the age class: %s with the error: %v\n", ageClass, err)
-            os.Exit(1)
-        }
-        defer rows.Close()
-
-        var tournaments []map[string]any
-        columns, err := rows.Columns()
-        if err != nil {
-            fmt.Println("Error getting columns:", err)
-            continue
-        }
-
-        for rows.Next() {
-            values := make([]any, len(columns))
-            valuePtrs := make([]any, len(columns))
-            for i := range columns {
-                valuePtrs[i] = &values[i]
-            }
-
-            if err := rows.Scan(valuePtrs...); err != nil {
-                fmt.Println("Error scanning row:", err)
-                continue
-            }
-
-            entry := make(map[string]any)
-            for i, col := range columns {
-                entry[col] = values[i]
-            }
-            tournaments = append(tournaments, entry)
-        }
-
-        if err := rows.Err(); err != nil {
-            fmt.Println("Error during rows iteration:", err)
-            continue
-        }
-
-        ageClasses[ageClass] = tournaments
-    }
-    
-    return ageClasses 
+func readTournamentsFromJSONFiles() string {
+    return ""
 }
 
 func insertLocalDataIntoRemoteDB() {
-    tournamentData := readTournamentsFromLocalDatabase()
-    fmt.Println(tournamentData)
-
     url := fmt.Sprintf("libsql://[%s].turso.io?authToken=[%s]", dbCredentials.DbName, dbCredentials.DbToken)
 
     libDB, err := sql.Open("libsql", url)
