bdd6c353eecb00dab3b1e7e6ddee489bb1e0642a
alexiiko <alexiiko.msl@gmail.com>
2025-03-17 17:25:48 +0100

    added search button and dropdown menu for age classes

--- src/frontend/lib/pages/search_tournaments_page.dart
+++ src/frontend/lib/pages/search_tournaments_page.dart
@@ -12,15 +12,76 @@ class SearchTournamentsPage extends StatefulWidget {
 class _SearchTournamentsPageState extends State<SearchTournamentsPage> {
     @override
     Widget build(BuildContext context) {
-        return Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Search Tournaments Page'),
-              ],
+    return (
+            Center(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [ 
+                    AgeClassesDropdownMenu(),
+                    SearchButton()
+                    ]
+                ),
+            )
+        );
+    }
+}
+
+class AgeClassesDropdownMenu extends StatefulWidget {
+    const AgeClassesDropdownMenu({super.key});
+
+    @override
+    State<AgeClassesDropdownMenu> createState() => _AgeClassesDropdownMenuState();
+}
+
+class _AgeClassesDropdownMenuState extends State<AgeClassesDropdownMenu> {
+    String currentAgeClass = "M18";
+    
+    @override
+    Widget build(BuildContext context) {
+        return DropdownButton<String>(
+            value: currentAgeClass,
+            onChanged: (String? otherAgeClass) {
+                setState(() {
+                    currentAgeClass = otherAgeClass!; 
+                });
+            },
+            items: <String>['M18', 'M16', 'M14', 'M13']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+            dropdownColor: Colors.white,
+            style: const TextStyle(color: Colors.black),
+        );
+    }
+}
+ 
+class SearchButton extends StatefulWidget {
+    const SearchButton({super.key});
+
+    @override
+    State<SearchButton> createState() => _SearchButtonState();
+}
+
+class _SearchButtonState extends State<SearchButton> {
+    onButtonPress() {
+        return {};
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return ElevatedButton(
+            onPressed: onButtonPress,
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                ),
             ),
-          ),
+            child: Icon(Icons.search),
         );
     }
 }
