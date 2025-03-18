33e155d9696b648d1ff5c866d2e821ba3f95a36c
alexiiko <123288428+alexiiko@users.noreply.github.com>
2025-03-18 10:24:30 +0100

    deleting frontend as it does not work properly

--- src/frontend/lib/db_service/db_models/local_settings_db.dart
+++
@@ -1,7 +0,0 @@
-class LocalSettingsDb {
-    String userStreet;
-    bool darkOrLightTheme;
-    bool sendNotifications;
-
-    LocalSettingsDb({required this.userStreet, required this.darkOrLightTheme, required this.sendNotifications});
-}
