e2fbe5d9d6bdcae28263a76d4bf8b9145230a977
alexiiko <alexiiko.msl@gmail.com>
2025-03-21 17:20:38 +0100

    made some changes to the database backend - added code snippet to delete all tournaments in every table - changed functionality that the start date of the tournament is the   deadline for the deletion (see dbConnection.go for further explanation) - changed some prints for better testing

--- src/backend/go/dbConnection.go
+++ src/backend/go/dbConnection.go
@@ -37,11 +37,23 @@ func main() {
 
     // read local tournament data from json files
     ageClasses := [12]string{"M11", "M12", "M13", "M14", "M16", "M18", "W11", "W12", "W13", "W14", "W16", "W18"}
-    
+
+    /*
+    code to delete all rows from every ageClass table
     for _, ageClass := range ageClasses {
+        _, err = libDB.Exec(fmt.Sprintf("DELETE FROM %s", ageClass))
+        if err != nil {
+            fmt.Println("Error deleting the table:", ageClass, err)
+        }
+        fmt.Println("deleted table: ", ageClass)
+    }
+    */
+
+    for _, ageClass := range ageClasses {
+        // add new tournaments
         linkRows, err := libDB.Query(fmt.Sprintf("SELECT link FROM %s", ageClass))
         if err != nil {
-            fmt.Println("Error selecting the links from the M11 table: ", err)
+            fmt.Println(fmt.Sprintf("Error selecting the links from the %s table: ", ageClass), err)
             os.Exit(1)
         }
 
@@ -73,6 +85,7 @@ func main() {
             os.Exit(1)
         }
         
+        // rewrite this part because is still adds tournaments that already are in the database
         for _, tournament := range tournaments {
             var tournamentAlreadyInDB bool = false
             for _, tournamentLink := range tournamentlinks {
@@ -88,15 +101,15 @@ func main() {
                     INSERT INTO %s (title, event_date, regis_start_date, regis_end_date, draw_date, street, plz, link)
                     VALUES (?, ?, ?, ?, ?, ?, ?, ?)
                 `, ageClass), tournament.Title, tournament.EventDate, tournament.RegisStart, tournament.RegisEnd, tournament.Draw, tournament.Street, tournament.PLZ, tournament.Link)
+                fmt.Println(fmt.Sprintf("Inserted data from json file: %s into table %s from remote db", ageClass, ageClass))
                 if err != nil {
                     fmt.Println("There was an error inserting the data into the remote DB:", err)
                     os.Exit(1)
                 }
-                fmt.Println(fmt.Sprintf("Inserted data from json file: %s into table %s from remote db", ageClass, ageClass))
-                fmt.Println()
             }
         }
-        
+
+        // delete tournaments that already occured
         idsRows, err := libDB.Query(fmt.Sprintf("SELECT id FROM %s", ageClass))
         if err != nil {
             fmt.Println("Error selecting the id and event_date from db rows: ", err)    
@@ -138,11 +151,14 @@ func main() {
         eventDatesIndex := 0
         for eventDatesIndex < len(eventDates) {
             currentId := ids[eventDatesIndex]
-            currentEventDate, err := time.Parse("02-01-2006", strings.Replace(eventDates[eventDatesIndex][13:], ".", "-", -1))
+            // tournament dates are stored like this in the database 16.03.2025 - 20.03.2025
+            // only the last date gets looked at when checking if the tournament already occured
+            currentEventDate, err := time.Parse("02-01-2006", strings.Replace(eventDates[eventDatesIndex][:10], ".", "-", -1))
             if err != nil {
                 fmt.Println("Error parsing the currentEventDate: ", err)
                 os.Exit(1)
             }
+
             currentDate, err := time.Parse("02-01-2006", time.Now().Format("02-01-2006"))
             if err != nil {
                 fmt.Println("Error parsing the current date: ", err)
@@ -161,6 +177,7 @@ func main() {
         tournamentsToBeDeletedIndex := 0 
         for tournamentsToBeDeletedIndex < len(tournamentsToBeDeleted) {
             _, err := libDB.Exec(fmt.Sprintf("DELETE FROM %s WHERE id = %s", ageClass, tournamentsToBeDeleted[tournamentsToBeDeletedIndex]))
+            fmt.Println(fmt.Sprintf("deleted tournament from %s with id %d", ageClass, tournamentsToBeDeletedIndex))
             if err != nil {
                 fmt.Println(fmt.Sprintf("Error deleting the tournament id %s from the %s table: ", tournamentsToBeDeleted[tournamentsToBeDeletedIndex], ageClass), err)
                 os.Exit(1)
