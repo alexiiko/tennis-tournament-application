58113c3bd089324fe3dff734128b494faef634ba
alexiiko <alexiiko.msl@gmail.com>
2025-03-26 15:34:59 +0100

    deleted temporary test file and added function to return the tournament data for a specific age class

--- src/frontend/lib/tournament_service/tournament_getter.dart
+++ src/frontend/lib/tournament_service/tournament_getter.dart
@@ -2,32 +2,44 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'db_credentials.dart';
 
-void returnTournamentsFromAgeClass (String ageClass) async {
-  final response = await http.post(
+Future<Map> returnTournamentData(String ageClass) async {
+    final response = await http.post(
     Uri.parse(url),
     headers: {
-      'Authorization': 'Bearer $authToken',
-      'Content-Type': 'application/json',
+          'Authorization': 'Bearer $authToken',
+          'Content-Type': 'application/json',
     },
     body: jsonEncode({
-      "requests": [
-        {"type": "execute", "stmt": {"sql": "SELECT link FROM $ageClass"}},
-        {"type": "close"},
-      ],
-    }),
-  );
+        "requests": [
+                {"type": "execute", "stmt": {"sql": "SELECT * FROM $ageClass"}},
+                {"type": "close"},
+            ],
+        }),
+    );
 
-  if (response.statusCode == 200) {
-    print(jsonDecode(response.body));
-  } else {
-    print('Error: ${response.statusCode}, ${response.body}');
-  }
+    if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+    } else {
+        return jsonDecode(response.body);
+    }
 }
 
-void main() {
+Future<List> returnTournamentDataFromAgeClass() async {
     var ageClassArray = ["M11", "M12", "M13", "M14", "M16", "M18"];
-    for (var i = 0; i < ageClassArray.length; i++) {
-        returnTournamentsFromAgeClass(ageClassArray[i]);
-        print("\n");
+    var result = await returnTournamentData(ageClassArray[0]);
+    var tournamentDataAPI = result["results"][0]["response"]["result"]["rows"];
+
+    var allTournaments = [];
+
+    var tournament = [];
+
+    for (var i = 0; i < tournamentDataAPI.length; i++) {
+        for (var j = 1; j < tournamentDataAPI[i].length; j++) {
+            tournament.add(tournamentDataAPI[i][j]["value"]);
+        }
+        allTournaments.add(tournament);
+        tournament = [];
     }
+    
+    return allTournaments;
 }
