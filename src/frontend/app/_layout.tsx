da7de5085b9a84c9b6dcaf4fe0b32b4ef6dabe0c
alexiiko <alexiiko.msl@gmail.com>
2025-05-05 17:42:42 +0200

    deleted frontend as it was a wrong build

--- src/frontend/app/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import {  Tabs } from "expo-router";
-
-export default function RootLayout() {
-  return <Tabs>
-        <Tabs.Screen name="searchTournaments" options={{ title: "Turniere suchen"}}/>
-        <Tabs.Screen name="savedTournaments" options={{ title: "gespeicherte Turniere"}}/>
-        <Tabs.Screen name="settings" options={{ title: "Einstellungen"}}/>
-    </Tabs>
-}
--- src/frontend/app/searchTournaments.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from "@react-navigation/elements";
-
-export default function main() {
-    return <Text> Hier sucht man Turniere</Text>
-}
