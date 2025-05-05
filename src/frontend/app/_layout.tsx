c3cfb8cc3962b28e0478584bba930e29e5620d7b
alexiiko <alexiiko.msl@gmail.com>
2025-05-05 18:43:51 +0200

    added icons to tabs and removed index from tab bar

--- src/frontend/app/_layout.tsx
+++ src/frontend/app/_layout.tsx
@@ -1,10 +1,11 @@
-import { Tabs } from "expo-router";
+import { Tabs } from "expo-router"
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 export default function RootLayout() {
     return <Tabs>
-
-    <Tabs.Screen name="searchTournaments" options={{ title:"Turniere suchen" }} />
-    <Tabs.Screen name="savedTournaments" options={{ title:"gespeicherte Turniere" }} />
-    <Tabs.Screen name="settings" options={{ title:"Einstellungen" }} />
+    <Tabs.Screen name="index" options={{ title:"index", href: null}} />
+    <Tabs.Screen name="settings" options={{ title:"Einstellungen", tabBarIcon: () => (<Ionicons name="settings-outline" size={24} color="black" />), tabBarActiveTintColor: "black"}} />
+    <Tabs.Screen name="searchTournaments" options={{ title:"Turniere suchen",  tabBarIcon: () => (<Ionicons name="search-outline" size={24} color="black"/>), tabBarActiveTintColor: "black"}} />
+    <Tabs.Screen name="savedTournaments" options={{ title:"gespeicherte Turniere" , tabBarIcon: () => (<Ionicons name="save-outline" size={24} color="black" />),tabBarActiveTintColor: "black"}}/>
     </Tabs>
 }
