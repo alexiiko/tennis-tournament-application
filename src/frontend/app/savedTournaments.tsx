7da88865b42a8c8cfc58237d1a6d8f0ee5a4c11b
alexiiko <alexiiko.msl@gmail.com>
2025-05-27 14:24:06 +0200

    added safeareaview

--- src/frontend/app/savedTournaments.tsx
+++ src/frontend/app/savedTournaments.tsx
@@ -1,5 +1,10 @@
 import { Text } from "@react-navigation/elements";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function main() {
-    return <Text>Hier sind gespeicherte Turniere</Text>
+    return (
+      <SafeAreaView>
+        <Text>Hier sind gespeicherte Turniere</Text>
+      </SafeAreaView>
+    )
 }
