171403ae13da4ea6dd74a20721fbdd5bd49b532d
alexiiko <alexiiko.msl@gmail.com>
2025-05-27 14:24:43 +0200

    changed ui - removed header - added search bar     - with search icon     - with filter icon     -> both are buttons - wrapped the page in a safeareaview

--- src/frontend/app/searchTournaments.tsx
+++ src/frontend/app/searchTournaments.tsx
@@ -1,5 +1,20 @@
 import { Text } from "@react-navigation/elements";
+import { retrieveDBData } from "../services/fetchDataDB.js";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Button } from "react-native-paper";
 
 export default function main() {
-    return <Text>Hier sucht man Turniere.</Text>
+    return (
+      <SafeAreaView>
+        <View>
+          <View style={{ borderWidth: 1, borderColor: "grey", padding: 10, borderRadius: 7, marginLeft: 5, marginRight: 5, }}>
+            <View style={{ justifyContent: "center" }}>
+                <Button icon="filter" textColor="black" buttonColor="transparent" style={{ position: "absolute", left: 0}}> </Button>
+                <Button icon="magnify" textColor="black" buttonColor="transparent" style={{ position: "absolute", right: 0}}> </Button>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    )
 }
