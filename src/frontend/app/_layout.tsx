c0e87bb4b11b647ed8df06a8e95a46cbe9870137
alexiiko <alexiiko.msl@gmail.com>
2025-05-27 14:23:12 +0200

    hid the header of each page

--- src/frontend/app/_layout.tsx
+++ src/frontend/app/_layout.tsx
@@ -3,9 +3,9 @@ import Ionicons from '@expo/vector-icons/Ionicons'
 
 export default function RootLayout() {
     return <Tabs>
-    <Tabs.Screen name="index" options={{ title:"index", href: null}} />
-    <Tabs.Screen name="searchTournaments" options={{ title:"Turniere suchen",  tabBarIcon: () => (<Ionicons name="search-outline" size={24} color="black"/>), tabBarActiveTintColor: "black"}} />
-    <Tabs.Screen name="savedTournaments" options={{ title:"gespeicherte Turniere" , tabBarIcon: () => (<Ionicons name="save-outline" size={24} color="black" />),tabBarActiveTintColor: "black"}}/>
-    <Tabs.Screen name="settings" options={{ title:"Einstellungen", tabBarIcon: () => (<Ionicons name="settings-outline" size={24} color="black" />), tabBarActiveTintColor: "black"}} />
+    <Tabs.Screen name="index" options={{ title:"index", href: null, headerShown: false}} />
+    <Tabs.Screen name="searchTournaments" options={{ title:"Turniere suchen",  tabBarIcon: () => (<Ionicons name="search-outline" size={24} color="black"/>), tabBarActiveTintColor: "black", headerShown: false}} />
+    <Tabs.Screen name="savedTournaments" options={{ title:"gespeicherte Turniere" , tabBarIcon: () => (<Ionicons name="save-outline" size={24} color="black" />),tabBarActiveTintColor: "black", headerShown: false}}/>
+    <Tabs.Screen name="settings" options={{ title:"Einstellungen", tabBarIcon: () => (<Ionicons name="settings-outline" size={24} color="black" />), tabBarActiveTintColor: "black", headerShown: false}} />
     </Tabs>
 }
