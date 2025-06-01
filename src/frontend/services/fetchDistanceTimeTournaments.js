fa72658b8cbd4a8072179e4ffdf9e7d4a97eaffe
alexiiko <alexiiko.msl@gmail.com>
2025-06-01 11:30:06 +0200

    added error handling - for getting coordinates of the user and tournament address - for calculating the distance and the duration between the 2 addresses

--- src/frontend/services/fetchDistanceTimeTournaments.js
+++ src/frontend/services/fetchDistanceTimeTournaments.js
@@ -2,23 +2,35 @@ import { orsURL, orsAuthToken, orsGEOCODEAuthToken } from "./serviceCredentials.
 
 async function getTournamentAddressCoordinates(tournamentAdress) {
   const orsAPIGeocodeResponse = await fetch(`https://api.openrouteservice.org/geocode/search?api_key=${orsGEOCODEAuthToken}&text=${encodeURIComponent(tournamentAdress)}`)
-  const data = await orsAPIGeocodeResponse.json()
-  const tournamentCoordinates = data.features[0].geometry.coordinates
-  return tournamentCoordinates
+  if (orsAPIGeocodeResponse.status != 200) {
+    return "Error fetching the tournament coordinates"
+  } else {
+    const data = await orsAPIGeocodeResponse.json()
+    const tournamentCoordinates = data.features[0].geometry.coordinates
+    
+    return tournamentCoordinates
+  }
 }
 
 async function getUserAdressCoordinates(userAddress) {
   const orsAPIGeocodeResponse = await fetch(`https://api.openrouteservice.org/geocode/search?api_key=${orsGEOCODEAuthToken}&text=${encodeURIComponent(userAddress)}`)
-  const data = await orsAPIGeocodeResponse.json()
-  const userCoordinates = data.features[0].geometry.coordinates
+  if (orsAPIGeocodeResponse.status != 200) {
+    return "Error fetching the user coordinates"
+  } else {
+    const data = await orsAPIGeocodeResponse.json()
+    const userCoordinates = data.features[0].geometry.coordinates
 
-  return userCoordinates
+    return userCoordinates
+  }
 }
 
 export async function retrieveDistanceAndTimeBetweenTournamentAndUser(userAddress, tournamentAddress) {
   const userAddressCoordinates = await getUserAdressCoordinates(userAddress)
   const tournamentAdressCoordinates = await getTournamentAddressCoordinates(tournamentAddress) 
   
+  if (Array.isArray(userAddressCoordinates) == false || Array.isArray(tournamentAdressCoordinates) == false) {
+    return "Error fetching the coordinates of the user or the tournament"
+  } else {
   const orsAPIResponse = await fetch(orsURL, {
     method: 'POST',
     headers: {
@@ -29,12 +41,15 @@ export async function retrieveDistanceAndTimeBetweenTournamentAndUser(userAddres
       coordinates: [userAddressCoordinates, tournamentAdressCoordinates],
     }),
   })
-
-  const data = await orsAPIResponse.json()
-  const timeAndDurationObject = data.routes[0].summary
-
-  const distanceToGetToTournament = Math.round(timeAndDurationObject.distance / 1000) // we divide by 1000 as the duration given by the api is in meters
-  const timeToGetToTournament = Math.round(timeAndDurationObject.duration / 60) // we divide by 60 as the duration given by the api is in seconds 
-
-  return [distanceToGetToTournament, timeToGetToTournament]
+    if (orsAPIResponse.status != 200) {
+      return "Error calculating the distance and time between the user and the tournament"
+    } else {
+        const data = await orsAPIResponse.json()
+        const timeAndDurationObject = data.routes[0].summary
+        
+        const distanceToGetToTournament = String(Math.round(timeAndDurationObject.distance / 1000)) // we divide by 1000 as the duration given by the api is in meters
+        const timeToGetToTournament = String(Math.round(timeAndDurationObject.duration / 60)) // we divide by 60 as the duration given by the api is in seconds 
+        return [distanceToGetToTournament, timeToGetToTournament]
+    }
+  }
 }
